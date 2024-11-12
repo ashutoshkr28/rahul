fd3930a0657c812b1e675c45f55e67d17b201fdb
ashutoshkr28 <ashutoshkumarmp3@gmail.com>
2024-11-12 15:51:45 +0530

    change

---
+++ components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <h1> Dr Rahul </h1>
+      <h2>Narayan Dental Clinic</h2>
+    </div>
+  )
+}
+
+export default Header
